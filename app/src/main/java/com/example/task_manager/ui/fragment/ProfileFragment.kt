f9ef156a8d955df7efd76ea34d61742a9022fd87
kalyinur <kadyrovnurik01@gmail.com>
2023-06-12 13:00:10 +0300

    Initial commit

---
+++ app/src/main/java/com/example/task_manager/ui/fragment/ProfileFragment.kt
@@ -0,0 +1,27 @@
+package com.example.task_manager.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.task_manager.databinding.FragmentNotificationsBinding
+import com.example.task_manager.databinding.FragmentProfileBinding
+
+
+class ProfileFragment : Fragment() {
+
+
+    private lateinit var binding : FragmentProfileBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentProfileBinding.inflate(inflater)
+        return binding.root
+    }
+
+
+
+}
